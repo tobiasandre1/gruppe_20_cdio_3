67fa80bf0f407526b997f4cdaecdeed65ce60e71
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-04-24 16:31:18 +0200

    Submitting username and password to login page returns the values

--- src/rest/Service.java
+++ src/rest/Service.java
@@ -1,6 +1,8 @@
 package rest;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 @Path("hello")
@@ -9,6 +11,11 @@ public class Service {
 	@GET
 	public String hello(){
 		return "hello";
+	}
+	
+	@POST
+	public String hello2(@FormParam("username") String username, @FormParam("password") String password){
+		return username+", "+password;
 	}
 	
 }
