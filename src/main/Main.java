b6debd2c1573695c9c093207b8c9e9fdd722cdba
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-05-01 13:30:04 +0200

    HALP MEH

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import datalayer.UserDAO;
+import idatalayer.IUserDAO;
+
+public class Main {
+
+	public static void main(String[] args) {
+		IUserDAO dataAccess = new UserDAO();
+
+	}
+
+}
