1b64e356b2ae51a9b5ba3ba35d0a97c75d753a5f
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-05-01 14:45:53 +0200

    Made HTML row builder

--- src/rest/HTMLBuilder.java
+++ src/rest/HTMLBuilder.java
@@ -6,9 +6,13 @@ public class HTMLBuilder {
 
 	public String buildRow(UserDTO user){
 		String html = "";
-		html += "<tr>";
-		
-		html += "</tr>";
-		return "";
+		html += "<tr>\n";
+		html += "<td>"+user.getUserId()+"</td>\n";
+		html += "<td>"+user.getUserName()+"</td>\n";
+		html += "<td>"+user.getIni()+"</td>\n";
+		html += "<td>"+user.getRoles()+"</td>\n";
+		html += "<td>"+user.getCpr()+"</td>\n";
+		html += "</tr>\n";
+		return html;
 	}
 }
