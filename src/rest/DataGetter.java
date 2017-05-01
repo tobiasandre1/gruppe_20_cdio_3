f85cb14aa0b6da784441a279cbe5fa326b3b9e1d
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-05-01 14:01:55 +0200

    AAAAAAA

---
+++ src/rest/DataGetter.java
@@ -0,0 +1,16 @@
+package rest;
+
+import javax.ws.rs.FormParam;
+//import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("datagetter")
+public class DataGetter {
+	
+	@POST
+	public String getData(@FormParam("username") String username, @FormParam("password") String password){
+		return "";
+		
+	}
+}
