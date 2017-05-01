6865f33d45cda45a7e061a9fea19c9bc5eeafea8
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-05-01 14:42:12 +0200

    Updates

--- src/rest/DataGetter.java
+++ src/rest/DataGetter.java
@@ -1,14 +1,14 @@
 package rest;
 
 import javax.ws.rs.FormParam;
-//import javax.ws.rs.GET;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 @Path("datagetter")
 public class DataGetter {
 	
-	@POST
+	@GET
 	public String getData(@FormParam("username") String username, @FormParam("password") String password){
 		return "";
 		
---
+++ src/rest/HTMLBuilder.java
@@ -0,0 +1,14 @@
+package rest;
+
+import dto.UserDTO;
+
+public class HTMLBuilder {
+
+	public String buildRow(UserDTO user){
+		String html = "";
+		html += "<tr>";
+		
+		html += "</tr>";
+		return "";
+	}
+}
