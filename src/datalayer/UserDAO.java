f0d953e543962e04f40202d3b9b9d037c7578391
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-05-08 15:27:03 +0200

    Fixed static data initialization bug

--- src/datalayer/UserDAO.java
+++ src/datalayer/UserDAO.java
@@ -8,11 +8,16 @@ import dto.UserDTO;
 
 public class UserDAO implements IUserDAO{
 	static List<UserDTO> userList = new ArrayList<UserDTO>();
-	
-	public UserDAO(){
-			createUser(new UserDTO(1, "admin", "a", new ArrayList<String>() {
+	static{
+		userList.add(new UserDTO(1, "admin", "A", new ArrayList<String>() {
 				private static final long serialVersionUID = -5718424645068011262L;
 				{add("admin");}}, "", "010517-0000"));
+		userList.add(new UserDTO(2, "test", "T", new ArrayList<String>() {
+			private static final long serialVersionUID = -5718424645068011262L;
+			{add("operatoer");}}, "", "080517-0000"));
+	}
+	
+	public UserDAO(){
 	}
 	
 	
