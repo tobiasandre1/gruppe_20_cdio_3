9ea8f9fd936f7e881c33930b2dd2d4cd8c76a99d
Theis Gadegaard <theis.gadegaard@gmail.com>
2017-05-10 14:40:46 +0200

    index.html virker ikke

--- src/rest/LoginVerification.java
+++ src/rest/LoginVerification.java
@@ -1,20 +1,27 @@
 package rest;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
 import datalayer.UserDAO;
 
 @Path("/LoginVerification")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+
 public class LoginVerification {
 
 	UserDAO userDAO = new UserDAO();
+	int response = 0;
 
 	@POST
-	public int verification(@FormParam("username") String username, @FormParam("password") String password){
+	public int verification(String username, String password){
 		for(int i = 0; i < userDAO.getUserList().size(); i++){
 			if((userDAO.getUser(i).getUserName() == username) && (userDAO.getUser(i).getPassword() == password)){
-				return 1;
+				response = 1;
+				return response;
 			}
 		} 
-		return 0;
+		return response;
 	}
 }
